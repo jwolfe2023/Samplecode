285f15c15eeb13b6ee3f821ab6a0660b49e9636f
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-04-29 09:17:39 -0400

    Sample code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        int output = 0;
+
+        int input1 = 0;
+
+        int input2 = 0
+
+//################################################# Start of the Program ###############################################
+
+//      They are first greeted with this screen
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+                                                                +");
+        System.out.println("+       1) Next Message                                          +");
+        System.out.println("+                                                                +");
+        System.out.println("+                                                                +");
+        System.out.println("+                                                                +");
+        System.out.println("+                                                                +");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.print("> ");
+        output = keyboard.nextInt();
+
+//################################################## Start of Section 1 ################################################
+
+            if (output == 1) {
+//              When 1 is selected it will prompt you to edit input 1 or 2
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("+                                                                +");
+                System.out.println("+       1) back                                                  +");
+                System.out.println("+                                                                +");
+                System.out.println("+                                                                +");
+                System.out.println("+                                                                +");
+                System.out.println("+                                                                +");
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.print("> ");
+                output = keyboard.nextInt();
+//
+
+                if (output == 1) {
+//          When 1 is selected it will return back to the last message
+
+
+                }
+            }
+        }
     }
-}
+
